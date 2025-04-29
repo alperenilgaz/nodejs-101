ae6e3b6ad17938517fe9895d064836f16984969f
alperenilgaz <alperenilgaz3@gmail.com>
2025-04-30 02:46:04 +0300

    form-data & routes module

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
 var http = require("http")
-var fs = require("fs")
+
+var routeHandler = require("./route") 
 const port = 3000
 
 
-var server = http.createServer((req,res) => {
-
-    if(req.url === '/'){
-        fs.readFile("index.html",(error,html) => {
-            res.writeHead(200,{"Content-Type":"text/html"})
-            res.write(html)
-            res.end()
-        })
-    }
-    else if(req.url==='/manager'){
-        fs.readFile("manager.html",(error,html) => {
-            res.writeHead(200,{"Content-Type":"text/html"})
-            res.write(html)
-            res.end()
-        })
-    }
-    else{
-        fs.readFile("404.html",(error,html) => {
-            res.writeHead(404,{"Content-Type":"text/html"})
-            res.write(html)
-            res.end()
-        })
-    }
-    
-})
+var server = http.createServer(routeHandler)
 
 
 server.listen(port)
